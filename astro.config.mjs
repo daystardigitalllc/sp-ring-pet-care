b59d418e6f5007fbe140e4877e7095f0ab8c2175
austin hayes <austin.hayes@tnsos.gov>
2026-08-11 14:32:55 -0500

    Pull and register the full Fulldev UI component library inside client project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwind()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
   },
 });
